11d583f7e8c9159591c4d7d9726ca46717c0722f
Alchemist28 <hypersonic_22@yahoo.com>
2016-02-07 15:26:14 +1030

    ModelPopUpDemoApp

---
+++ ModelPopUpAjaxToolKitApp/ModelPopUpAjaxToolKitApp/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelPopUpAjaxToolKitApp
+{
+    public class Helper
+    {
+        public static string  ConnectionString
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.ConnectionStrings["ModelPopUP"].ConnectionString;
+            }
+        }
+    }
+}
